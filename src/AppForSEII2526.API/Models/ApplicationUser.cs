bb18432e436abdb7a154fe9faa48b95c3a6fc95a
César <Csar@DESKTOP-QJB0BLI>
2025-09-29 20:20:43 +0200

    Clase Review creada con su respectivo DbSet

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -5,3 +5,15 @@ namespace AppForSEII2526.API.Models;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser {
 }
+
+public class Review { 
+        public String country { get; set; }
+        public DateTime created { get; set; }
+
+       public String drivertype { get; set; }
+
+       public int Id { get; set; }
+
+         public String UserName { get; set; }
+}
+
