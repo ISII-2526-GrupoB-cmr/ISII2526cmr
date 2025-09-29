e8d9ce88caaf4016d32f3b6bdaa6548e756a167c
manumoratalla <moratallagonzalezmanuel@gmail.com>
2025-09-29 20:14:58 +0200

    añadidos atributos en clase model

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -4,4 +4,11 @@ namespace AppForSEII2526.API.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser {
+
 }
+
+public class Model {
+    public int ID { get; set; };
+    public string Name { get; set; };
+}
+
