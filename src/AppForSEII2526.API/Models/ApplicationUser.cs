78458fccd9250eb70ff929b4ea5b091fd2d8ad14
César <Csar@DESKTOP-QJB0BLI>
2025-09-30 19:06:19 +0200

    Se arregla el error de tener la clase review dentro de ApplicationUser.cs, creando la clase Rerview.cs para ese cometido

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -6,14 +6,4 @@ namespace AppForSEII2526.API.Models;
 public class ApplicationUser : IdentityUser {
 }
 
-public class Review { 
-        public String country { get; set; }
-        public DateTime created { get; set; }
-
-       public String drivertype { get; set; }
-
-       public int Id { get; set; }
-
-         public String UserName { get; set; } 
-}
 
