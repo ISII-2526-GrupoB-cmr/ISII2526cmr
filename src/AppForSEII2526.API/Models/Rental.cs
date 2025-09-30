389575ed88231c692c455dd3aa27ed95cf005ea8
ROBERTO MORENO GÓMEZ <Roberto.Moreno3@alu.uclm.es>
2025-09-30 10:42:42 -0700

    ya he añadido todos los atributos de la clase Rental

---
+++ src/AppForSEII2526.API/Models/Rental.cs
@@ -0,0 +1,12 @@
+public class Rental
+{   
+    public string DeliveryCarDealer { get; set; }
+    public DateTime EndDate { get; set; }
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string PaymentMethod { get; set; }
+    public DateTime RentignDate { get; set; }
+    public DateTime StartDate { get; set; }
+    public string Surname { get; set; }
+    public float TotalPrice { get; set; }
+}
