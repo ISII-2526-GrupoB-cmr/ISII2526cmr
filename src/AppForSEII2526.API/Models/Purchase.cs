2df60e9e1e822ad6ef97b382c644dc15b79b8896
manumoratalla <moratallagonzalezmanuel@gmail.com>
2025-09-30 19:52:48 +0200

    creada clase Purchase

---
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -0,0 +1,19 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Purchase
+    {
+        public int DeliveryCarLeader { get; set; }
+        
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string PaymentMethod { get; set; }
+
+        public DateTime PurchasingDate { get; set; }
+
+        public string PurchasingType  { get; set; }
+
+        public string Surname { get; set; }
+    }
+}
