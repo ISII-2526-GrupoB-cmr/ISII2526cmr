2f0aee38f3df15e2dbeff13c5bc2b86bbabed976
César <Csar@DESKTOP-QJB0BLI>
2025-09-30 19:29:40 +0200

    Clase ReviewItem creada con sus atributos

---
+++ src/AppForSEII2526.API/Models/ReviewItem.cs
@@ -0,0 +1,10 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class ReviewItem
+    {
+        public int CarId { get; set; }
+        public String Description { get; set; } 
+        public int Rating { get; set; }
+        public int ReviewId { get; set; }
+    }
+}
