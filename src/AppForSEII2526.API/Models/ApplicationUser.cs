120c456f0c8f0d21735405ba6d346eac4d02d1df
ROBERTO MORENO GÓMEZ <Roberto.Moreno3@alu.uclm.es>
2025-09-30 10:05:29 -0700

    hemos creado la clase car con todos sus atributos

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -7,24 +7,3 @@ public class ApplicationUser : IdentityUser {
     
 }
 
-public class car
-{
-    public string CarClass { get; set; }
-    public string Color { get; set; }
-    public string Description { get; set; }
-    public string EngDisplacement { get; set; }
-    public string FuelType { get; set; }
-    public int Id { get; set; }
-    public string MaintenanceType { get; set; }
-    public string Manufacturer { get; set; }
-    public int PurchaseItems { get; set; }
-    public float PurchasePrice { get; set; }
-    public float QuantityForPurchase { get; set; }
-    public float QuantityForRenting { get; set; }
-    
-    public float RentingPrice { get; set; }
-    public float ReviewItems { get; set; }
-    public float RimSize { get; set; } 
-
-
-}
