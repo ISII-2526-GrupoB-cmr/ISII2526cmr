42b08f254e491797b75f38d2f5fa00501d842acb
manumoratalla <moratallagonzalezmanuel@gmail.com>
2025-10-01 09:32:22 +0200

    añadidos atributos a ApplicationUser

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -6,5 +6,11 @@ namespace AppForSEII2526.API.Models;
 public class ApplicationUser : IdentityUser {
 
 
+    public int ID { get; set; }
+
+    public string Name { get; set; }
+
+    public string Surname { get; set; }
+
 }
 
