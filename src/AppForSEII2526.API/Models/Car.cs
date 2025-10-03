281c555c104e2b877ed0778a4c923eab026c9a1b
manumoratalla <moratallagonzalezmanuel@gmail.com>
2025-10-03 12:46:35 +0200

    confirmacion relacion creada car y purchaseitem

--- src/AppForSEII2526.API/Models/Car.cs
+++ src/AppForSEII2526.API/Models/Car.cs
@@ -10,17 +10,18 @@ public class Car
     public int Id { get; set; }
     public string MaintenanceType { get; set; }
     public string Manufacturer { get; set; }
-    public int PurchaseItems { get; set; }
+    public int PurchaseItem { get; set; }
     public float PurchasePrice { get; set; }
     public float QuantityForPurchase { get; set; }
     public float QuantityForRenting { get; set; }
 
     public float RentingPrice { get; set; }
-    public float RentalItems { get; set; }
+    public float RentalItem { get; set; }
     public float RimSize { get; set; }
 
     public IList<ReviewItem> ReviewItems { get; set; }
     public IList<RentalItem> RentalItems { get; set; }
+    public IList<RentalItem> PurchaseItems { get; set; }
 
     public Model Model { get; set; }
 
