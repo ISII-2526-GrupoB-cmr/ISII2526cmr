3a3b1e263ac2dcf12936fe4c66e1e09ea9e3f368
manumoratalla <moratallagonzalezmanuel@gmail.com>
2025-10-07 17:20:23 +0200

    añadidas restricciones a clase purchase

--- src/AppForSEII2526.API/Models/Purchase.cs
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -2,15 +2,30 @@
 {
     public class Purchase
     {
-        public int DeliveryCarLeader { get; set; }
 
-        public string PaymentMethod { get; set; }
+        public int Id { get; set; }
+
+        [Display(Name = "Delivery Car Leader")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please, set your address for delivery")]
+
+        public string DeliveryCarLeader { get; set; }
+
+        [Display(Name = "Payment Method")]
+        public PaymentMethodTypes PaymentMethod { get; set; }
 
         public DateTime PurchasingDate { get; set; }
 
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Please, set your purchasing type")]
         public string PurchasingType  { get; set; }
 
-
         public IList<PurchaseItem> PurchaseItems { get; set; }
+
+
+    }
+
+    public enum PaymentMethodTypes
+    {
+        GooglePay,
+        Visa
     }
 }
