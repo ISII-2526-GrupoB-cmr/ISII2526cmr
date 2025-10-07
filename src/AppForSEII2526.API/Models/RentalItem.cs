349d320d6a3451fb452e3b0db2f25dbf1259dcb9
robermg332 <Roberto.Moreno3@alu.uclm.es>
2025-10-07 17:34:19 +0200

    En este no hay muchos cambios ya que no hay que poner ninguna restriccion sobre los atributos

--- src/AppForSEII2526.API/Models/RentalItem.cs
+++ src/AppForSEII2526.API/Models/RentalItem.cs
@@ -1,8 +1,15 @@
+namespace AppForSEII2526.API.Models { 
 [PrimaryKey(nameof(CarId), nameof(RentalId))]
 public class RentalItem {
     public int CarId { get; set; }
+
     public int Quantity { get; set; }
+
     public int RentalId { get; set; }
+
     public Car Car { get; set; }
+
     public Rental Rental { get; set; }
+
+}
 }
