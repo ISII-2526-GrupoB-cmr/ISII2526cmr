78024a744ca3d3231acfbb586045ae917b6bedd6
robermg332 <Roberto.Moreno3@alu.uclm.es>
2025-10-08 09:47:26 +0200

    He añadido las restricciones para el atributo Quantity

--- src/AppForSEII2526.API/Models/RentalItem.cs
+++ src/AppForSEII2526.API/Models/RentalItem.cs
@@ -1,9 +1,10 @@
 namespace AppForSEII2526.API.Models { 
 [PrimaryKey(nameof(CarId), nameof(RentalId))]
 public class RentalItem {
-    public int CarId { get; set; }
 
-    public int Quantity { get; set; }
+    public int CarId { get; set; }
+    [Range(1,int.MaxValue, ErrorMessage = "Quantity must be at least 1")]
+        public int Quantity { get; set; }
 
     public int RentalId { get; set; }
 
