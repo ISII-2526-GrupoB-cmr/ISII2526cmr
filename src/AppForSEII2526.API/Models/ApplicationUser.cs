5417113f506e0f7f3c96e3b78f0d55e36b241a0b
manumoratalla <moratallagonzalezmanuel@gmail.com>
2025-10-08 09:52:44 +0200

    añadidas relaciones en clase ApplicationUser

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -5,12 +5,15 @@ namespace AppForSEII2526.API.Models;
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser {
 
-
-    public int ID { get; set; }
+    public string UserName { get; set; }
 
     public string Name { get; set; }
 
     public string Surname { get; set; }
 
-}
+    public IList<Purchase> Purchases { get; set; }
 
+    public IList<Rental> Rentals { get; set; }
+
+    public IList<Review> Reviews { get; set; }
+}
