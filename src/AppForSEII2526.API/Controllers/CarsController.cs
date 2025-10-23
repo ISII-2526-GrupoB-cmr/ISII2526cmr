42cfb26ed00650642b6b9518cc517dbb5a1d885e
manumoratalla <moratallagonzalezmanuel@gmail.com>
2025-10-23 11:12:53 +0200

    Controlador creado con su correspondiente DTO

---
+++ src/AppForSEII2526.API/Controllers/CarsController.cs
@@ -0,0 +1,48 @@
+﻿using AppForSEII2526.API.DTOs.Car;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        //used to enable your controller to access to the database
+        private readonly ApplicationDbContext _context;
+        //used to log any information when your system is running
+        private readonly ILogger<CarsController> _logger;
+
+        public CarsController(ApplicationDbContext context, ILogger<CarsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<Car>), (int)HttpStatusCode.OK)]
+
+        public async Task<ActionResult> GetCars()
+        {
+            IList<Car> cars = await _context.Cars.ToListAsync();
+            return Ok(cars);
+        }
+
+
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<CocheParaComprarDTO>), (int)HttpStatusCode.OK)]
+
+        public async Task<ActionResult> GetCoche_Para_ComprarDTO(string? filtroColor, string? modelo)
+        {
+            var coches = await _context.Cars.Include(coche=>coche.Model)
+                .Where(c=> c.Color.Contains(filtroColor) || (filtroColor==null) && (c.Model.Name.Contains(modelo) || modelo == null)
+            )
+                .Select(c=>new CocheParaComprarDTO(c.Id, c.Model.Name,
+                c.Color, c.FuelType, c.Manufacturer, c.PurchasePrice)).ToListAsync();
+            return Ok(coches);
+        }
+    }
+}
---
+++ src/AppForSEII2526.API/DTOs/Car/CocheParaComprarDTO.cs
@@ -0,0 +1,27 @@
+﻿namespace AppForSEII2526.API.DTOs.Car
+{
+    public class CocheParaComprarDTO
+    {
+        public int Id { get; set; }
+
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+        public string FuelType { get; set; }
+
+        public string Manufacturer { get; set; }
+
+        public float PurchasePrice { get; set; }
+
+        public CocheParaComprarDTO(int id, string model, string color, string fuelType, string manufacturer, float purchasePrice)
+        {
+            Id = id;
+            Model = model;
+            Color = color;
+            FuelType = fuelType;
+            Manufacturer = manufacturer;
+            PurchasePrice = purchasePrice;
+        }
+    }
+}
