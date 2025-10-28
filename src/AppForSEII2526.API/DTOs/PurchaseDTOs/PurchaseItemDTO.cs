2fb274c013f98c92d353a1570b260d073cadf883
manumoratalla <moratallagonzalezmanuel@gmail.com>
2025-10-28 18:39:35 +0100

    Realizadas primeras lineas de codigo del controlador y DTOs para revisar los detalles de la compra

---
+++ src/AppForSEII2526.API/DTOs/PurchaseDTOs/PurchaseItemDTO.cs
@@ -0,0 +1,40 @@
+﻿using System.Drawing;
+
+public class PurchaseItemDTO
+{
+    public PurchaseItemDTO(int carID, float purchasePrice, float totalPrice, string modelo, string carColor, string description = "")
+    {
+        CarID = carID;
+        CarColor = carColor;
+        PurchasePrice = purchasePrice;
+        TotalPrice = totalPrice;
+        Modelo = modelo;
+        Description = description;
+    }
+
+    public int CarID { get; set; }
+    public string CarColor { get; set; }
+
+    public float TotalPrice { get; set; }
+
+    public double PurchasePrice { get; set; }
+
+    public string? Description { get; set; }
+
+    public string Modelo { get; set; }
+
+    public override bool Equals(object? obj)
+    {
+        return obj is PurchaseItemDTO dTO &&
+               CarID == dTO.CarID &&
+               CarColor == dTO.CarColor &&
+               PurchasePrice == dTO.PurchasePrice &&
+               Description == dTO.Description &&
+               Modelo == dTO.Modelo;
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(CarID, CarColor, PurchasePrice, Description, Modelo);
+    }
+}
