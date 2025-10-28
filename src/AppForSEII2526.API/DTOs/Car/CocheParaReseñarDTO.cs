9b9a2cf0a5be6b04fb7e2073f601843ceb3347e6
César <Csar@DESKTOP-QJB0BLI>
2025-10-28 08:22:25 +0100

    Controlador hecho, DTO para create, y cambiados los atributos de ApplicationDbContext por public

---
+++ src/AppForSEII2526.API/DTOs/Car/CocheParaReseñarDTO.cs
@@ -0,0 +1,31 @@
+﻿namespace AppForSEII2526.API.DTOs.Car
+{
+    public class CocheParaReseñarDTO
+    {
+
+        public string carClass { get; set; }
+        public int Id { get; set; }
+
+        public string Model { get; set; }
+
+        public string Color { get; set; }
+
+        public string FuelType { get; set; }
+
+        public string Manufacturer { get; set; }
+
+       
+
+        public CocheParaReseñarDTO(int id, string model, string color, string fuelType, string manufacturer, string CarClass)
+        {
+            Id = id;
+            Model = model;
+            Color = color;
+            FuelType = fuelType;
+            Manufacturer = manufacturer;
+            carClass = CarClass;
+           
+        }
+
+    }
+}
