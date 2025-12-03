7f8b44375bb4c9e73b91890ad4accd7441c2407a
CÉSAR GÓMEZ TARANCÓN <Cesar.Gomez3@alu.uclm.es>
2025-12-03 10:03:14 +0100

    Añadida línea de código faltante en Program.cs

--- src/AppForSEII2526.Web/Program.cs
+++ src/AppForSEII2526.Web/Program.cs
@@ -38,6 +38,7 @@ builder.Services.AddSingleton<IEmailSender<AppForSEII2526.Web.Data.ApplicationUs
 
 string? URI2API = builder.Configuration.GetValue(typeof(string), "AppForSEII2526_API") as string;
 
+builder.Services.AddScoped<AppForSEII2526APIClient>(sp => new AppForSEII2526APIClient(URI2API, new HttpClient()));
 
 var app = builder.Build();
 
