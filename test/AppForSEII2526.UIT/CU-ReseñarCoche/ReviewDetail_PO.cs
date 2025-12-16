34a25cd37eb2ce304df5bbad180c09b38472e430
CÉSAR GÓMEZ TARANCÓN <Cesar.Gomez3@alu.uclm.es>
2025-12-16 17:48:54 +0100

    Creadas las pruebas funcionales para el detail de reseñar coche, aunque puede que con errores

---
+++ test/AppForSEII2526.UIT/CU-ReseñarCoche/ReviewDetail_PO.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSeII2526.UIT.ReviewDetail
+{
+    public class ReviewDetail_PO : PageObject
+    {
+        public  ReviewDetail_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+
+        public bool CheckReviewDetail(string username, string country, DateTime created, Enum drivertype)
+        {
+            WaitForBeingVisible(By.Id("TotalPrice"));
+            bool result = true;
+            result = result && _driver.FindElement(By.Id("Username")).Text.Contains(username);
+            result = result && _driver.FindElement(By.Id("Country")).Text.Contains(country);
+            result = result && _driver.FindElement(By.Id("Created")).Text.Contains(created.ToString("g"));
+            result = result && _driver.FindElement(By.Id("DriverType")).Text.Contains(drivertype.ToString());
+
+
+
+
+            return result;
+
+        }
+
+        public bool CheckListOfCars(List<string[]> expectedReviewItems)
+        {
+            return CheckBodyTable(expectedReviewItems, By.Id("ReviewCars"));
+        }
+    }
+}
