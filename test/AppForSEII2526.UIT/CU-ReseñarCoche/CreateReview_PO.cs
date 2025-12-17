1f9c270bc72f86bc6e76ad93c1331409a8626d4b
César <Csar@DESKTOP-QJB0BLI>
2025-12-17 19:33:16 +0100

    Creadas las pruebas funcionales para el caso de uso Review

---
+++ test/AppForSEII2526.UIT/CU-ReseñarCoche/CreateReview_PO.cs
@@ -0,0 +1,100 @@
+﻿//CreateReview_PO
+
+
+using OpenQA.Selenium.DevTools.V140.Network;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.CU_ReseñarCoche
+{
+    internal class CreateReview_PO : PageObject
+    {
+
+        private By _username = By.Id("Username");
+        private By _country = By.Id("Country");
+        private By _driverType = By.Id("DriverType");
+
+        private By _modifyBttn = By.Id("ModifyReviewCart");
+
+        private By _submitReviewbttn = By.Id("Submit");
+        private By _reviewTable = By.Id("TableOfReviewItems");
+
+
+        private IWebElement username() => _driver.FindElement(_username);
+        private IWebElement country() => _driver.FindElement(_country);
+        private IWebElement driverType() => _driver.FindElement(_driverType);
+
+
+        public CreateReview_PO(IWebDriver driver, ITestOutputHelper output)
+            : base(driver, output)
+        {
+        }
+
+
+        public void RellenarReview(string username, string country, Enum driverType)
+        {
+            WaitForBeingVisible(_username);
+
+            this.username().Clear();
+            this.username().SendKeys(username);
+            this.country().Clear();
+            this.country().SendKeys(country);
+            this.driverType().SendKeys(driverType.ToString());
+        }
+
+        public void RellenarDetailReview(string reviewDescription, int Reviewrating, string model)
+        {
+            var descriptionInput = _driver.FindElement(
+                By.Id($"description_{model}")
+            );
+            descriptionInput.Clear();
+            descriptionInput.SendKeys(reviewDescription);
+
+            var ratingSelect = new SelectElement(
+                _driver.FindElement(By.Id($"rating_{model}"))
+            );
+            ratingSelect.SelectByValue(Reviewrating.ToString());
+        }
+
+
+
+        public void PulsarSubmitReview()
+        {
+            _driver.FindElement(_submitReviewbttn).Click();
+        }
+
+        public void PulsarModifyCar()
+        {
+
+            _driver.FindElement(_modifyBttn).Click();
+        }
+
+
+
+
+        public bool ComprobarReviewItemsTable(List<string[]> expectedReviewItems)
+        {
+            return CheckBodyTable(expectedReviewItems, _reviewTable);
+        }
+
+        public bool CheckErrorMessageForMandatoryFields()
+        {
+
+            Thread.Sleep(1000);
+
+
+            var errores = _driver.FindElements(By.ClassName("validation-message"));
+
+
+            var sumarioErrores = _driver.FindElements(By.ClassName("validation-errors"));
+
+
+            return errores.Any(e => !string.IsNullOrWhiteSpace(e.Text)) ||
+                   sumarioErrores.Any(e => !string.IsNullOrWhiteSpace(e.Text));
+        }
+
+    }
+}
--- test/AppForSEII2526.UIT/CU-ReseñarCoche/ReviewDetail_PO.cs
+++ test/AppForSEII2526.UIT/CU-ReseñarCoche/ReviewDetail_PO.cs
@@ -1,24 +1,27 @@
-﻿using System;
+﻿//ReviewDetail_PO.cs
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace AppForSeII2526.UIT.ReviewDetail
+namespace AppForSeII2526.UIT.CU_ReseñarCoche
 {
     public class ReviewDetail_PO : PageObject
     {
-        public  ReviewDetail_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        public ReviewDetail_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
         {
         }
 
         public bool CheckReviewDetail(string username, string country, DateTime created, Enum drivertype)
         {
-            WaitForBeingVisible(By.Id("TotalPrice"));
+            Thread.Sleep(1000);
             bool result = true;
-            result = result && _driver.FindElement(By.Id("Username")).Text.Contains(username);
-            result = result && _driver.FindElement(By.Id("Country")).Text.Contains(country);
-            result = result && _driver.FindElement(By.Id("Created")).Text.Contains(created.ToString("g"));
+            result = result && _driver.FindElement(By.Id("NameSurname")).Text.Contains(username);
+            result = result && _driver.FindElement(By.Id("Country")).Text.Trim().ToLower().Contains(country.ToLower());
+            var actualReviewDate = DateTime.Parse(_driver.FindElement(By.Id("Created")).Text);
+            result = result && ((actualReviewDate - actualReviewDate) < new TimeSpan(0, 1, 0));
             result = result && _driver.FindElement(By.Id("DriverType")).Text.Contains(drivertype.ToString());
 
 
